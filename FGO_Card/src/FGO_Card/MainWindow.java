94d44c954e1c2e3431915046e19d520d4f5c28f9
NightKirie <qwer55113322@gmail.com>
2018-06-01 12:00:05 +0800

    lol

--- FGO_Card/src/FGO_Card/MainWindow.java
+++ FGO_Card/src/FGO_Card/MainWindow.java
@@ -7,6 +7,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 
@@ -32,18 +35,27 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+		setTitle("FGO Card");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MainWindow.class.getResource("/Images/Icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 480, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JButton button = new JButton("New button");
-		contentPane.add(button, BorderLayout.CENTER);
+		JButton skillButton = new JButton("");
+		skillButton.setSelectedIcon(new ImageIcon(MainWindow.class.getResource("/Images/Icon.png")));
+		skillButton.setIcon(new ImageIcon(MainWindow.class.getResource("/Images/Skill_Btn.png")));
+		skillButton.setBounds(253, 150, 127, 159);
+		///skillButton.setOpaque(false);
+		//skillButton.setContentAreaFilled(false);
+		//skillButton.setBorderPainted(false);
+		contentPane.add(skillButton);
 		
-		JButton btnNewButton = new JButton("New button");
-		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		JLabel Menu_BackGround = new JLabel("");
+		Menu_BackGround.setIcon(new ImageIcon(MainWindow.class.getResource("/Images/MainBackgroun.png")));
+		Menu_BackGround.setBounds(0, 0, 464, 681);
+		contentPane.add(Menu_BackGround);
 	}
-
 }
