2ba7f89136c5edb4ce831936067a4b6b26c5a145
cicero884 <cicero884@gmail.com>
2018-06-01 15:27:35 +0800

    push test battle cant compile now!

---
+++ FGO_Card/src/battle/Battle.java
@@ -0,0 +1,27 @@
+package battle;
+import skill.*;
+import character.*;
+//import character.Caster;
+import card_total.*;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+public class Battle extends JFrame{
+	Character character;
+	Skill[] skill;
+	int difficulty;
+
+	Battle(){
+		character=new Caster();
+		skill=new Skill[]{new Aoe(),new Boost(),new Heal()};
+		difficulty=1;
+	}
+	Battle(Character c,Skill[] s,int d){
+		character=c;
+		skill=s;
+		difficulty=d;
+	
+	}
+
+}
