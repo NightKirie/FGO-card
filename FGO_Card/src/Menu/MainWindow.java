d17445f4da7ee814d3e0f5f05cd3e64c0f6ad39e
NightKirie <qwer55113322@gmail.com>
2018-06-02 21:54:34 +0800

    test ssh

--- FGO_Card/src/Menu/MainWindow.java
+++ FGO_Card/src/Menu/MainWindow.java
@@ -23,7 +23,7 @@ public class MainWindow extends JFrame {
 
 	/**********
 	  Launch the application.
-	 *******/
+	 ****/
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
