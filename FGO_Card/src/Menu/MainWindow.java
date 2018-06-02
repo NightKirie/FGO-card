78f423d803ab85f69b6bc7d7040691d1238eda26
NightKirie <qwer55113322@gmail.com>
2018-06-02 21:52:18 +0800

    test

--- FGO_Card/src/Menu/MainWindow.java
+++ FGO_Card/src/Menu/MainWindow.java
@@ -21,9 +21,9 @@ public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+	/**********
+	  Launch the application.
+	 *******/
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
