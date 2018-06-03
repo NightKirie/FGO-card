58b413ed7f4aadd5ffe06fc92b3c6eb6241e3b65
rainwind <max7400641@gmail.com>
2018-06-03 18:27:55 +0800

    add some player,monster detail add some item

---
+++ FGO_Card/src/card/Potion.java
@@ -0,0 +1,29 @@
+package card;
+import javax.swing.ImageIcon;
+public class Potion extends Item {
+	public String potiontype;
+	public Potion(String type,int size)
+	{
+		hp=size;
+		potiontype=type;
+		if(type.equals("heal"))
+		{
+			cardPicture=new ImageIcon("healpotion.jpg");
+		}
+		setIcon(cardPicture);
+	}
+
+	@Override
+	public void effect(Player input) {
+		// TODO Auto-generated method stub
+		if(potiontype.equals("heal"))
+		{
+			input.hp+=this.hp;
+			if(input.hp>input.maxhp)
+			{
+				input.hp=input.maxhp;
+			}
+		}
+	}
+
+}
---
+++ FGO_Card/src/card/Trap.java
@@ -0,0 +1,12 @@
+package card;
+
+public class Trap extends Item {
+
+
+	@Override
+	public void effect(Player input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
