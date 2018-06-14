c11149879dd97d51b1ed2b3c47dfcd45a48b0a88
cicero884 <cicero884@gmail.com>
2018-06-14 13:59:59 +0800

    rename some code,adding action Listener

--- FGO_Card/src/battle/Battle.java
+++ FGO_Card/src/battle/Battle.java
@@ -8,17 +8,24 @@ import card.*;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.JFrame;
-import java.util.Random;
+import java.awt.geom.Point2D;
 
-public class Battle extends JFrame{
-	int size,difficulty;
+public class Battle extends JPanel{
+	static int size;
+	int difficulty;
 	randomCard generater;
 
-	public static Card map[][];
+	public static Cardi[][] map;
+	public static Card player;
+	static image
 
 	public Battle(){this(1);}
 	public Battle(int difficulty){
 		setSize(485,748);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBackground(Color.BLACK);
+		setLayout(null);
+
 		size=3;
 		map=new Card[size][];
 		generater=new randomCard(difficulty);
@@ -27,10 +34,11 @@ public class Battle extends JFrame{
 			for(int j=0;j<size;++j){
 				if(i==1&&j==1){
 					switch(character.ChooseCharacter.getChosenID()){
-						case 1:map[1][1]=new card.Caster(character.ChooseCharacter.getChosenMaxHP());break;
-						case 2:map[1][1]=new card.Assassin(character.ChooseCharacter.getChosenMaxHP());break;
-						case 3:map[1][1]=new card.Lancer(character.ChooseCharacter.getChosenMaxHP());break;
+						case 1:player=new card.Caster(character.ChooseCharacter.getChosenMaxHP());break;
+						case 2:player=new card.Assassin(character.ChooseCharacter.getChosenMaxHP());break;
+						case 3:player=new card.Lancer(character.ChooseCharacter.getChosenMaxHP());break;
 					}
+					map[i][j]=player;
 				}
 				else{
 					map[i][j]=generater.nextCard();
@@ -39,5 +47,10 @@ public class Battle extends JFrame{
 			}
 		}
 	}
-
+	public static Point getLocation(Card target){
+		for(int i=0;i<size;++i){
+			for(int j=0;j<size;++j) if(target==map[i][j]) return new Point(i,j);
+		}
+		return null;
+	}
 }
