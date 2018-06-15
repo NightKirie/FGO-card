e04df8fd0d89938a8cbd345516beec38efed84e2
cicero884 <cicero884@gmail.com>
2018-06-15 23:31:11 +0800

    moveCard rule finished

--- FGO_Card/src/battle/Battle.java
+++ FGO_Card/src/battle/Battle.java
@@ -14,7 +14,7 @@ import java.awt.geom.Point2D;
 public class Battle extends JPanel{
 	static int size;
 	int difficulty;
-	randomCard generater;
+	static randomCard generater;
 
 	public static Card[][] map;
 	public static Card player;
@@ -52,11 +52,36 @@ public class Battle extends JPanel{
 		for(int i=0;i<size;++i){
 			for(int j=0;j<size;++j) if(target==map[i][j]) return new Point(i,j);
 		}
+		System.out.println("error:cannot find card in map");
 		return null;
 	}
 	public static void swapCard(Point a,Point b){
 		Card tmp=map[a.x][a.y];
 		map[a.x][a.y]=map[b.x][b.y];
 		map[b.x][b.y]=tmp;
+	}
+	static Point add(Point a,Point b){return new Point(a.x+b.x,a.y+b.y);}
+	static boolean inField(Point a){return a.x<size&&a.x>=0&&a.y<size&&a.y>=0;}
+	final static Point[] relation={new Point(0,1),new Point(1,0),new Point(0,-1),new Point(-1,0)}; 
+	public static void moveCard(Point position,int direction){
+		//0:up,1:right,2:down,3:left
+		//when use this function,the card on next direction will be remove
+		int backDirection=(direction+2)%4;
+		Point target=add(position,relation[direction]);
+		for(Point i=position;inField(i);i=add(i,relation[backDirection])){
+			map[target.x][target.y]=map[i.x][i.y];
+			target=i;
+		}
+		if(target.equals(position)){//need move other card if player move from side
+			do{//check where the next card need to be move
+				direction=(direction+1)%4;
+				position=add(position,relation[direction]);
+			}while(!inField(position));
+			for(Point i=position;inField(i);i=add(i,relation[backDirection])){
+				map[target.x][target.y]=map[i.x][i.y];
+				target=i;
+			}
+		}
+		map[target.x][target.y]=generater.nextCard();
 	}
 }
