89660a40a84c394f17cf71426468586080db97d7
cicero884 <cicero884@gmail.com>
2018-06-21 09:30:38 +0800

    try fix cd bug

--- FGO_Card/src/card/Card.java
+++ FGO_Card/src/card/Card.java
@@ -40,7 +40,10 @@ public class Card extends JButton{
 			System.out.println(Card.this.getText()+"  "+Location);
 			if(Card.this instanceof Object){
 				if(Card.this instanceof Monster){
-					if(direction>=0) ((Player)field.player).attack((Creature)Card.this);
+					if(direction>=0){
+						((Player)field.player).attack((Creature)Card.this);
+						field.updateStatus();
+					}
 				}
 				else{//Item
 					if(Card.this instanceof Weapon){
@@ -48,22 +51,29 @@ public class Card extends JButton{
 						field.remove(Card.this);
 						if(direction>=0) field.moveCard(playerLocation,direction);
 						else field.addCard(new Empty(),Location);
+						field.updateStatus();
 					}
 					else if(Card.this instanceof Potion){
 						((Potion)Card.this).effect((Player)field.player);
 						field.remove(Card.this);
 						if(direction>=0) field.moveCard(playerLocation,direction);
 						else field.addCard(new Empty(),Location);
+						field.updateStatus();
 					}
-					else if((Card.this instanceof Bomb)&&direction>=0) field.swapCard(Location,playerLocation);
+					else if((Card.this instanceof Bomb)&&direction>=0){
+						field.swapCard(Location,playerLocation);
+						field.updateStatus();
+					}
 					else if(Card.this instanceof Coin){
 						field.pickGold(((Object)Card.this).hp);
 						field.remove(Card.this);
 						if(direction>=0)field.moveCard(playerLocation,direction);
 						else field.addCard(new Empty(),Location);
+						field.updateStatus();
 					}
 					else if(Card.this instanceof Chest){
 						((Chest)Card.this).open();
+						field.updateStatus();
 					}
 				}
 			}
@@ -71,9 +81,9 @@ public class Card extends JButton{
 				if(direction>=0){
 					field.remove(Card.this);
 					field.moveCard(playerLocation,direction);
+					field.updateStatus();
 				}
 			}
-			if(direction>=0||!(Card.this instanceof Empty)) field.updateStatus();
 		}
 	} 
 }
